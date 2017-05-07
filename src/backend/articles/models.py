ff14edd5cd85d3a1c1fbcbe116b4d493c81e1392
Luthfi <vlootfie@gmail.com>
2017-05-07 23:38:26 +0000

    Add Tag class to article's model

--- src/backend/articles/models.py
+++ src/backend/articles/models.py
@@ -1,6 +1,37 @@
+from datetime import datetime
+
+import markdown as markdown
 from django.contrib.auth.models import User
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+    article = models.ForeignKey(Article)
+
+    class Meta:
+        verbose_name = _("Tag")
+        verbose_name_plural = _("Tags")
+        unique_together = (('tag', 'article'),)
+        index_together = [['tag', 'article'], ]
+
+    def __str__(self):
+        return self.tag
+
+    @staticmethod
+    def get_popular_tags():
+        tags = Tag.objects.all()
+        count = {}
+        for tag in tags:
+            if tag.article.status == Article.PUBLISHED:
+                if tag.tag in count:
+                    count[tag.tag] = count[tag.tag] + 1
+                else:
+                    count[tag.tag] = 1
+        sorted_count = sorted(count.items(), key=lambda t: t[1], reverse=True)
+        return sorted_count[:20]
 
 
 class Article(models.Model):
@@ -25,3 +56,40 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            super(Article, self).save(*args, **kwargs)
+        else:
+            self.updated_at = datetime.now()
+        if not self.slug:
+            slug_str = "%s %s" % (self.pk, self.title.lower())
+            self.slug = slugify(slug_str)
+        super(Article, self).save(*args, **kwargs)
+
+    def get_content_as_markdown(self):
+        return markdown.markdown(self.content, safe_mode='escape')
+
+    @staticmethod
+    def get_published():
+        articles = Article.objects.filter(status=Article.PUBLISHED)
+        return articles
+
+    def create_tags(self, tags):
+        tags = tags.strip()
+        tag_list = tags.split(' ')
+        for tag in tag_list:
+            if tag:
+                t, created = Tag.objects.get_or_create(tag=tag.lower(), article=self)
+
+    def get_tags(self):
+        return Tag.objects.filter(article=self)
+
+    def get_summary(self):
+        if len(self.content) > 255:
+            return '{0}...'.format(self.content[:255])
+        else:
+            return self.content
+
+    def get_summary_as_markdown(self):
+        return markdown.markdown(self.get_summary(), safe_mode='escape')
