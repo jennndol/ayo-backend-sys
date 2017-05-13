55282ea8c96d02f0d95a2a426b9f13ea08f4576d
Luthfi <vlootfie@gmail.com>
2017-05-13 22:46:31 +0000

    Refactor article serializers

--- src/backend/articles/serializers.py
+++ src/backend/articles/serializers.py
@@ -1,7 +1,8 @@
 from .models import Article
 from rest_framework import serializers
 
+
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Article
-		fields = ('title','slug','content','status','created_by','created_at','updated_at')
+    class Meta:
+        model = Article
+        fields = ('title', 'slug', 'content', 'status', 'created_by', 'created_at', 'updated_at')
