8c196963967208365a31fe1bbf7cf19c7eff3c24
Luthfi <vlootfie@gmail.com>
2017-05-13 16:29:33 +0000

    Add profile class into authentication app

--- src/backend/authentication/models.py
+++ src/backend/authentication/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        db_table = 'auth_profile'
+
+    def __str__(self):
+        return self.user.username
+
+    def get_bio(self):
+        return self.bio
