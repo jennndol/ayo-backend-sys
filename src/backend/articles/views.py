eb578a27d64d646b56076e8c08aa412b1c8665aa
Luthfi <vlootfie@gmail.com>
2017-05-13 22:47:07 +0000

    Refactor article views

--- src/backend/articles/views.py
+++ src/backend/articles/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from .models import Article
 from rest_framework import viewsets
+
+from .models import Article
 from .serializers import ArticleSerializer
 
+
 class ArticleViewSet(viewsets.ModelViewSet):
-	"""
-	API endpoint that allows articles to be viewed or edited.
-	"""
-	queryset = Article.objects.all().order_by('-created_at')
-	serializer_class = ArticleSerializer
+    """
+    API endpoint that allows articles to be viewed or edited.
+    """
+    queryset = Article.objects.all().order_by('-created_at')
+    serializer_class = ArticleSerializer
