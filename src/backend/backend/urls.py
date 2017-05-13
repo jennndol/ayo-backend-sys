8f893be2d37f725dcd2bcaccae968bd400ecf6d1
Luthfi <vlootfie@gmail.com>
2017-05-13 22:30:02 +0000

    Set rest_framework's router

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -20,5 +20,10 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from articles import views
 
+router = routers.DefaultRouter()
+router.register(r'articles', views.ArticleViewSet)
+
 urlpatterns = [
+	url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
