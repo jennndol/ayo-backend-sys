cd6e9cda1cc98e0d6e47fb2fda55ca7d48d23a1a
Luthfi <vlootfie@gmail.com>
2017-05-14 23:10:15 +0000

    Add create and update function into ArticleSerializer class

--- src/backend/articles/serializers.py
+++ src/backend/articles/serializers.py
@@ -1,8 +1,27 @@
-from .models import Article
 from rest_framework import serializers
 
+from .models import Article
 
-class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('title', 'slug', 'content', 'status', 'created_by', 'created_at', 'updated_at')
+
+class ArticleSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+    def create(self, validated_data):
+        """
+        Create and return a new Article instance, given the validated data
+        :param validated_data: 
+        :return: Article instance
+        """
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        :param instance: 
+        :param validated_data: 
+        :return: instance
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.title)
+        instance.save()
+        return instance
