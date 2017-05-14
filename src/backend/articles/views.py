faa4a0ca171da03549f2f419afbfbfbd2f5fe1fa
Luthfi <vlootfie@gmail.com>
2017-05-14 23:46:39 +0000

    Add perform_create method to automatically add user who add the new article

--- src/backend/articles/views.py
+++ src/backend/articles/views.py
@@ -24,6 +24,9 @@ class ArticleList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
 
 class ArticleDetail(APIView):
     """
